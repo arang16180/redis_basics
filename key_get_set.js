242dfea7ceaf8733f3cc664630db7c33f339d8bf
Angel Rangel <105447969+arang16180@users.noreply.github.com>
2023-09-18 20:33:38 -0600

    Add files via upload

---
+++ key_get_set.js
@@ -0,0 +1,22 @@
+var redis = require("redis")
+// var client = redis.createClient()
+var client = redis.createClient({
+    legacyMode: true
+});
+
+client.connect('error', err => {
+    console.log('Redis err')
+    console.log(err)
+   })
+
+client.set('my_key', "hello World!")
+client.get('my_key', function(err, reply) {
+    console.log(reply);
+})
+
+client.mset('header', 0,'left', 0,'article', 0,'right', 0,'footer', 0)
+client.mget(['header', 'left', 'article', 'right', 'footer'], function(err, value){
+    console.log(value);
+   
+})
+// client.quit()
